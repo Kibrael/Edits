eaf246abaff4dbfc4bdb8359d2483ad4f76fe619
David Roell <breeroell@gmail.com>
2016-07-15 16:58:33 -0400

    adding first macro edit SQL

---
+++ Q080.sql
@@ -0,0 +1,29 @@
+﻿
+WITH denom AS(
+	SELECT
+	agency
+	,concat(agency, RID) AS arid
+	,count(sequence) as denom_count
+	FROM hmdalar2014
+	WHERE property_type IN ('1', '2')
+	AND action IN ('1', '2', '3', '4', '5')
+	GROUP BY 
+	agency, concat(agency, RID)),
+
+numer AS (
+	SELECT
+	agency
+	,concat(agency, RID) AS arid
+	,count(sequence) as numer_count
+	FROM hmdalar2014
+	WHERE property_type IN ('1', '2')
+	AND action IN ('1', '2', '3', '4', '5')
+	AND ethnicity IN ('3', '4')
+	GROUP BY agency, concat(agency, RID))
+
+SELECT
+numer.arid
+,(numer_count::real / denom_count::real) *100 as Q080
+FROM numer
+LEFT JOIN denom ON numer.arid = denom.arid
+--WHERE numer_count::real / denom_count::real > 50
---
+++ Q081.sql
@@ -0,0 +1,24 @@
+﻿WITH numer AS (SELECT
+agency
+,concat(agency, RID) AS arid
+,count(sequence) AS numer_count
+FROM hmdalar2014
+WHERE 
+action IN ('1', '2', '3')
+AND property_type IN ('1', '2')
+AND race IN ('6', '7')
+GROUP BY agency, concat(agency, RID)),
+
+denom AS (SELECT
+agency
+,concat(agency, RID) AS arid
+,count(sequence) AS denom_count
+FROM hmdalar2014
+WHERE 
+action IN ('1', '2', '3', '4', '5')
+GROUP BY agency, concat(agency, RID))
+
+SELECT numer_count, denom_count, (numer_count/denom_count::real) *100
+FROM numer
+LEFT JOIN denom ON numer.arid = denom.arid
+WHERE (numer_count/denom_count::real) *100 >50
