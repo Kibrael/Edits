4c1b0be9813ca0d8e70e4332a2083cc9fc37ebf6
David Roell <breeroell@gmail.com>
2016-07-18 13:30:58 -0400

    Q056, Q061 SQL

--- macro_sql.py
+++ macro_sql.py
@@ -551,6 +551,29 @@ GROUP BY agency, CONCAT(agency,rid))
 SELECT
 numer.agency, numer.arid, numer_count, denom_count, (numer_count/denom_count::REAL)*100
 FROM numer LEFT JOIN denom ON numer.arid = denom.arid
-WHERE (numer_count/denom_count::REAL)*100 > 5
-"""}
+WHERE (numer_count/denom_count::REAL)*100 > 5"""}
+
+Q061 = {"""WITH numer AS (SELECT
+agency
+,CONCAT(agency, rid) AS arid
+,COUNT(sequence) AS numer_count
+FROM hmdalar2014
+WHERE property_type = '1'
+AND lien = '1'
+AND action = '1'
+AND rate_spread::REAL >5
+AND rate_spread NOT LIKE '%NA%'
+GROUP BY agency, CONCAT(agency,rid)),
+
+denom AS (SELECT
+agency
+,CONCAT(agency,rid) AS arid
+,COUNT(sequence) AS denom_count
+FROM hmdalar2014
+GROUP BY agency, CONCAT(agency,rid))
+
+SELECT
+numer.agency, numer.arid, numer_count, denom_count, (numer_count/denom_count::REAL)*100
+FROM numer LEFT JOIN denom ON numer.arid = denom.arid
+WHERE (numer_count/denom_count::REAL)*100 >1"""}
 
