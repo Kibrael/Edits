fb55c3070e99fff82d2f2f484f4b14942925d4c2
David Roell <breeroell@gmail.com>
2016-07-27 16:11:25 -0400

    fix q071

--- Q071.sql
+++ Q071.sql
@@ -1,24 +1,13 @@
-﻿WITH thresh AS (SELECT
-agency
-,CONCAT(agency,rid) AS arid
-,COUNT(sequence) AS thresh_ct
-FROM hmdalar2014
-WHERE action IN ('1','6')
-AND loan_purpose IN ('1','3')
-AND property_type IN ('1','2')
-AND loan_type = '2'
-AND purchaser = '2'
-GROUP BY agency, CONCAT(agency,rid)),
-
-numer AS (SELECT
+﻿WITH numer AS (SELECT
 agency
 ,CONCAT(agency,rid) AS arid
 ,COUNT(sequence) AS sold
 FROM hmdalar2014
-WHERE action ='6'
-AND property_type In ('1','2')
-AND loan_type = '2'
-AND purchaser = '2'
+WHERE action IN ('1','6')
+AND loan_purpose IN ('1','3')
+AND property_type IN ('1','2')
+AND loan_type = '1'
+AND purchaser IN ('2')
 GROUP BY agency, CONCAT(agency,rid)),
 
 numer_prev AS (SELECT
@@ -26,17 +15,42 @@ agency
 ,CONCAT(agency,rid) AS arid
 ,COUNT(sequence) AS sold_prev
 FROM hmdalar2013
-WHERE action = '6'
+WHERE action IN ('1','6')
+AND property_type In ('1','2')
+AND loan_type = '1'
+AND purchaser IN ('2')
+GROUP BY agency, CONCAT(agency,rid)),
+
+denom AS (SELECT
+agency
+,CONCAT(agency,rid) AS arid
+,COUNT(sequence) AS count_curr
+FROM hmdalar2014
+WHERE action IN ('1','6')
+AND loan_purpose IN ('1','3')
 AND property_type IN ('1','2')
-AND loan_type = '2'
-AND purchaser = '2'
+AND loan_type = '1'
+GROUP BY agency, CONCAT(agency,rid)),
+
+denom_prev AS (SELECT
+agency
+,CONCAT(agency,rid) AS arid
+,COUNT(sequence) AS count_prev
+FROM hmdalar2013
+WHERE action IN ('1','6')
+AND loan_purpose IN ('1','3')
+AND property_type IN ('1','2')
+AND loan_type = '1'
 GROUP BY agency, CONCAT(agency,rid))
 
 SELECT
-thresh.arid, thresh.thresh_ct, sold_prev, sold, ((sold-sold_prev)/sold_prev::REAL)*100 AS pct_change
-FROM thresh LEFT JOIN numer ON numer.arid = thresh.arid 
-LEFT JOIN numer_prev ON thresh.arid = numer_prev.arid
-WHERE (CASE WHEN thresh_ct <2500 AND ABS(((sold-sold_prev)/sold_prev::REAL)*100) >10 THEN 1
-	    --WHEN thresh_ct >=2500 AND ABS(((sold-sold_prev)/sold_prev::REAL)*100) >=30 THEN 1
-	    ELSE NULL END) IS NOT NULL
+numer.agency, numer.arid, sold_prev, count_prev, sold, count_curr, (sold_prev/count_prev::REAL)*100 AS pct_sold_prev, (sold/count_curr::REAL)*100 AS pct_sold, 
+((sold-sold_prev)/sold_prev::REAL)*100 AS pct_change
+FROM numer LEFT JOIN numer_prev ON numer.arid = numer_prev.arid 
+LEFT JOIN denom ON denom.arid = numer.arid
+LEFT JOIN denom_prev ON denom_prev.arid = numer.arid
+
+--WHERE (CASE WHEN ABS(sold_prev/count_prev - sold/count_curr) > 10 THEN 1
+--	    WHEN count_curr >=2500 AND ABS(((sold/count_curr::REAL)*100)) >30 THEN 1
+--	    ELSE NULL END) IS NOT NULL
 ORDER BY pct_change asc
