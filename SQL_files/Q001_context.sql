d6084618ca3edb9010997a6721ddfd1cf6b08f7c
David Roell <breeroell@gmail.com>
2016-09-09 17:06:06 -0400

    beginning distribution analysis

---
+++ SQL_files/Q001_context.sql
@@ -0,0 +1,19 @@
+﻿SELECT
+
+SUM(amount::INT) AS sum_amt
+,AVG(amount::INT) AS avg_amt
+,STDDEV(amount::INT) AS std_amt
+,SUM(income::INT) AS sum_income
+,AVG(income::INT) AS avg_income
+,STDDEV(income::INT) AS std_income
+,SUM(amount::INT) / SUM(income::REAL) AS avg_mult
+,STDDEV(amount::INT / income::REAL) AS STD_mult 
+--check against distributions of income and amount to see if this query should be run against bins
+
+FROM hmdalar2014
+
+WHERE 
+amount NOT ILIKE '%NA%'
+AND income NOT ILIKE '%NA%'
+AND loan_purpose = '1'
+AND property_type = '1'
