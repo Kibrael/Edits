63aff91567d7d65f26029f3614ac1c89c2d4e71f
David Roell <breeroell@gmail.com>
2016-09-12 09:56:14 -0400

    adding min/max to desc stats sql

--- SQL_files/Q006.sql
+++ SQL_files/Q006.sql
@@ -1,19 +1,20 @@
 ﻿WITH numer AS (SELECT
 agency
 ,CONCAT(agency,rid) AS arid
-,COUNT(sequence) as numer_count
-FROM {table}
-WHERE action = '1' AND loan_purpose = '1'
-GROUP BY agency, CONCAT(agency, RID)),
+,COUNT(sequence) AS numer_count
+FROM hmdalar2014
+WHERE action = '1'
+GROUP BY agency, CONCAT(agency, rid)),
 
 denom AS (SELECT
 agency
-,CONCAT(agency, RID) AS arid
-,COUNT(sequence) as denom_count
-FROM {table}
-WHERE loan_purpose = '1'
-GROUP BY agency, CONCAT(agency, rid))
+,CONCAT(agency,rid) AS arid
+,COUNT(sequence) AS denom_count
+FROM hmdalar2014
+WHERE action IN ('1','2','3','4','5','6')
+GROUP BY agency, CONCAT(agency,rid))
 
-SELECT numer.agency, numer.arid, (numer_count/denom_count::REAL) *100 AS Q006
-FROM numer LEFT JOIN denom on numer.arid = denom.arid
-WHERE (numer_count/denom_count::REAL) * 100 > 95 AND numer_count > 25;
+SELECT
+numer.agency, numer.arid, (numer_count/denom_count::REAL)*100  AS Q010
+FROM numer LEFT JOIN denom ON numer.arid = denom.arid
+WHERE (numer_count/denom_count::REAL) *100 < 20;
